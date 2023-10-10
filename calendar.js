6b9d4197a7003bcbc362e9b61d009a26cc2809ce
Hazrody <116826998+Hazrody@users.noreply.github.com>
2023-10-10 15:19:34 +0200

    generate documentation with jsdoc

--- calendar.js
+++ calendar.js
@@ -9,19 +9,35 @@ const monthNames = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juil
  */
 function createCalendar(month, year) {
     if (month > 0 && month <= 12) {
+        /**
+         * Nombre de jours dans un mois
+         * @type {number}
+         */
         const nbDaysInMonth = new Date(year, month, 0).getDate();
-        console.log('Nombre de jours dans le mois:', nbDaysInMonth);
-
+        /**
+         * Premier jour du mois
+         * @type {number}
+         */
         const firstDayInMonth = new Date(year, month - 1, 1).getDay();
-        console.log('Premier jour du mois:', firstDayInMonth);
-
+        /**
+         * Nom du mois demandé
+         * @type {string}
+         */
         const currentNameMonth = monthNames[month - 1];
         console.log('Nom du mois demandé:', currentNameMonth);
-
+        /**
+         * Template du calendrier
+         * @type {string}
+         */
         let generatedHtmlCalendar = `<h2>${currentNameMonth} ${year}</h2><table><thead><tr><th>Dim</th><th>Lun</th><th>Mar</th><th>Mer</th><th>Jeu</th><th>Ven</th><th>Sam</th></tr></thead><tbody>`;
-
+        /**
+         * Jour
+         * @type {number}
+         */
         let day = 1;
-        // Ici 6 correspond au nombre de semaines possibles dans un mois si le mois commence un autre jour que dimanche
+        /**
+         * Ici 6 correspond au nombre de semaines possibles dans un mois si le mois commence un autre jour que dimanche
+         */
         for (let i = 0; i < 6; i++) {
             if (day > nbDaysInMonth) {
                 break;
@@ -39,8 +55,14 @@ function createCalendar(month, year) {
         }
 
         generatedHtmlCalendar += "</tbody></table>";
+        /**
+         * On retourne le calendrier pret a être affiché
+         */
         return generatedHtmlCalendar;
     } else {
+        /**
+         * Dans ce cas le mois entré est incorrect
+         */
         console.error('Le mois est incorrect');
     }
 }
